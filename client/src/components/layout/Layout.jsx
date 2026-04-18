e6606368b6c2947ee827fab4133d7b596852ff90
Swapnil Durgam <swapnildurgam89@gmail.com>
2026-04-18 17:56:36 +0530

    layout.jsx code modified/fixed

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -9,10 +9,11 @@ const Layout = () => {
   return (
     <div className="min-h-screen bg-neutral-50">
       <Navbar onMenuClick={() => setSidebarOpen(true)} />
-      <div className="flex">
+      <div className="flex relative">
         <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-        <main className="flex-1 md:ml-60 min-h-[calc(100vh-4rem)] p-3 sm:p-4 md:p-6">
-          <div className="max-w-7xl mx-auto page-enter">
+        {/* On mobile: full width. On desktop: offset by sidebar width */}
+        <main className="w-full md:ml-56 min-h-[calc(100vh-3.5rem)] p-3 sm:p-5 md:p-6 overflow-x-hidden">
+          <div className="max-w-7xl mx-auto">
             <Outlet />
           </div>
         </main>
