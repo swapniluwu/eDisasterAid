17dfba4d0b864b067070583c5bcf5060e1e431c5
Swapnil Durgam <swapnildurgam89@gmail.com>
2026-04-28 01:15:01 +0530

    layout fixed of navbar

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -9,9 +9,8 @@ const Layout = () => {
   return (
     <div className="min-h-screen bg-neutral-50">
       <Navbar onMenuClick={() => setSidebarOpen(true)} />
-      <div className="flex relative">
+      <div className="flex pt-14 sm:pt-16"> {/* Add padding-top here */}
         <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-        {/* On mobile: full width. On desktop: offset by sidebar width */}
         <main className="w-full md:ml-56 min-h-[calc(100vh-3.5rem)] p-3 sm:p-5 md:p-6 overflow-x-hidden">
           <div className="max-w-7xl mx-auto">
             <Outlet />
